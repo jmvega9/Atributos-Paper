e9489455091025c38b693b281684e5dba2040ec0
jmvega9 <65459405+jmvega9@users.noreply.github.com>
2020-06-04 16:36:26 -0500

    Atributos Paper

---
+++ Requerimientos.java
@@ -0,0 +1,116 @@
+/*
+ * implemente un algoritmo que le permita ingresar por teclado los paradigmas
+de la programacion orientada a objetos
+1. Atributos Funcionales
+2. Atributos no Funcionales
+3. Objetos del mundo real
+4. Objetos del mundo Informatico.
+ */
+package requerimientos;
+
+/**
+ *
+ * @author JESSICA MARIBEL VEGA MASAPANTA
+ */
+import java.util.Scanner;
+public class Requerimientos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Scanner objeto=new Scanner (System.in);
+
+    System.out.println("                           TEMA");
+    System.out.println("Deteccion de fallas e identificacion de motores automotrices");
+
+    System.out.println("\n\n              1. Atributos funcionales");
+    String funcionales=objeto.nextLine();
+
+    System.out.println("\n\n              2. Atributos no funcionales");
+    String nofuncionales=objeto.nextLine();
+
+    System.out.println("\n\n              3. Objetos del mundo real");
+    String real=objeto.nextLine();
+
+    System.out.println("\n 3.1 Tipo de Objetos");
+    String tipo=objeto.nextLine();
+
+    System.out.println("\n\n              4. Objetos del mundo informatico");
+
+    System.out.println("\n 4.1 Objeto 1 ");
+    String objeto1=objeto.nextLine();
+
+    System.out.println("\n 4.1.2 Atributos");
+
+    System.out.println(" Atributo 1");
+    String atributo1=objeto.nextLine();
+
+    System.out.println(" Atributo 2");
+    String atributo2=objeto.nextLine();
+
+    System.out.println(" Atributo 3");
+    String atributo3=objeto.nextLine();
+
+    System.out.println("\n 4.1.3 Servicios");
+
+    System.out.println(" Servicio 1");
+    String servicio1=objeto.nextLine();
+
+    System.out.println(" Servicio 2");
+    String servicio2=objeto.nextLine();
+
+    System.out.println(" Servicio 3");
+    String servicio3=objeto.nextLine();
+
+
+    System.out.println("\n\n 4.2 Objeto 2");
+    String objeto2=objeto.nextLine();
+
+    System.out.println("\n 4.2.1 Atributos");
+
+    System.out.println(" Atributo 1");
+    String atributo21=objeto.nextLine();
+
+    System.out.println(" Atributo 2");
+    String atributo22=objeto.nextLine();
+
+    System.out.println(" Atributo 3");
+    String atributo23=objeto.nextLine();
+
+    System.out.println("\n 4.2.3 Servicios");
+
+    System.out.println(" Servicio 1");
+    String servicio21=objeto.nextLine();
+
+    System.out.println(" Servicio 2");
+    String servicio22=objeto.nextLine();
+
+    System.out.println(" Servicio 3");
+    String servicio23=objeto.nextLine();
+
+
+    System.out.println("\n\n 4.3 Objeto 3");
+    String objeto3=objeto.nextLine();
+
+    System.out.println("\n 4.3.1 Atributos");
+
+    System.out.println(" Atributo 1");
+    String atributo31=objeto.nextLine();
+
+    System.out.println(" Atributo 2");
+    String atributo32=objeto.nextLine();
+
+    System.out.println("\n 4.3.3 Servicios");
+
+    System.out.println(" Servicio 1");
+    String servicio31=objeto.nextLine();
+
+    System.out.println(" Servicio 2");
+    String servicio32=objeto.nextLine();
+
+
+// TODO code application logic here
+    }
+
+}
